260cd75e68e982174a121ea12795d07c883d827e
jwandekoken <julio.wandekoken@paketa.com.br>
2021-06-05 13:57:34 -0300

    updated server file

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const FAVORITE_INSTRUCTOR = process.env.NAME;
+
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function main() {
-  while(true) {
-    console.log('Containers rule!');
+  while (true) {
+    console.log(`${FAVORITE_INSTRUCTOR} rocks`);
     await sleep(5000);
   }
 }
